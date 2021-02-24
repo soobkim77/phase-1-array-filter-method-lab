8a2d75ff5f7b03633eb34a5fb2b078aa024e1840
Soo-Beom <soobeomkim77@gmail.com>
2021-02-24 10:03:07 -0800

    commit tree 480fb8f9cf46c5e5c9f59cb7d3f62f2ce363f133

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string){
+   return drivers.filter(name => name.toLowerCase() == string.toLowerCase() );
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter (name => name.startsWith(string) == string.startsWith(string));
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(drivers => drivers.name == string)
+}
